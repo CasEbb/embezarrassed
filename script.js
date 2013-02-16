fadc690ca2f305d955cd6143507cb83ae74c2f63
Pavel Lishin <pavel.lishin@gmail.com>
2013-02-16 02:33:10 -0500

    Now can add account transfers

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ var printable = "";
 for(var i = 33; i <= 64; i++) {
     printable = printable + String.fromCharCode(i);
 }
+var spinner = "_/|\\";
 
 function add_account_row() {
     // three digits only
@@ -32,6 +33,68 @@ function add_account_row() {
     row.find("td.rrcl").text(rrcl);
     row.find("td.to_pk").text(to_pk);
     $("table.accounts").append(row);
+}
+
+function add_transfer() {
+
+    var account = "USD " + digits.random(4) + "-" + alphabet.concat(digits).random(6)
+        + " - CHF " + alphabet.concat(digits).random(9);
+    var transaction_id = digits.random(4) + "-" + alphabet.concat(digits).random(9);
+
+    var transaction = $("div.transfer.template").clone().removeClass("template");
+    transaction.find("div.account").text(account);
+    transaction.find("div.transaction").text(transaction_id);
+    transaction.find('div.progressbar div.fill').data('width', 0);
+    transaction.css("top", Math.floor(Math.random() * (window.innerHeight - transaction.height())));
+    transaction.css("left", Math.floor(Math.random() * (window.innerWidth - transaction.width())));
+    $("body").append( transaction );
+
+    var wait = $.Deferred();
+
+    var increment = function() {
+        // increment the percentage by some amount
+        var percent = parseInt(transaction.find("div.progressbar div.fill").data('width'));
+        var increase = Math.floor(Math.random() * 10);
+        percent = percent + increase;
+
+        transaction.find("div.progressbar div.fill").data('width', percent)
+        transaction.find("div.progressbar div.fill").animate( { width: percent + "%" }, 100 );
+        transaction.find("div.transferring span.percent").text(percent + "%");
+
+        // increment password guess
+        var current = transaction.find("div.password span.password").text();
+        transaction.find("div.password span.password").text( current + "*" );
+
+        if (percent >= 100) {
+            wait.resolve();
+        } else {
+            setTimeout( increment, Math.random() * 1000 );
+        }
+    }
+
+    var spin = setInterval( function() {
+        // flip the indicator
+        var current = transaction.find("span.rotate").text();
+        var next = spinner[ (spinner.indexOf( current ) + 1) % spinner.length ];
+        transaction.find("span.rotate").text( next );
+    }, Math.floor(Math.random() * 300) );
+
+    var blink = setInterval( function() {
+        // blink the password toggle
+        transaction.find(".blink").toggle();
+    }, 500);
+
+
+    increment();
+
+    wait.done( function() {
+        clearInterval(spin);
+        transaction.fadeOut(100, function() {
+            transaction.remove();
+            clearInterval(blink);
+        });
+    });
+
 }
 
 
@@ -64,3 +127,4 @@ $(document).ready( function() {
     });
 
 });
+
